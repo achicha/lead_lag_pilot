19b2e53613ff7d034cfcf282fa1dc0003d551588
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2019-04-25 13:25:01 +0200

    Working profutabke strategy before improvements

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -15,16 +15,16 @@ from trader.managers import MarginPerformance, StrategyPersistence
 # from amplify_charts import chart
 # from strategy.utils.tools import get_trade_events
 
-
 class Strategy():
     
-    def __init__(self, lead_exchange_name, lag_exchange, time_diff="10s"):
+    def __init__(self, lead_exchange_name, lag_exchange, time_diff="10s", min_profit=0.0004):
         self.lag_exchange = lag_exchange
         self.lead_exchange_name = lead_exchange_name
         self.lag_exchange_name = lag_exchange.exchange
         self.time_diff = time_diff
-        self.trade_locked = False
+        self.target_percentage = (self.lag_exchange.taker_fee * 2.0) +  min_profit
         
+        self.trade_locked = False
         self.lead_price = None
         self.lag_price = None
         self.last_lag_trade = None
@@ -43,55 +43,8 @@ class Strategy():
         self.lead_dataframe = pd.DataFrame(
             self.lead_price_list
         ).set_index("datetime", drop=True).last(self.time_diff)
-        min_price = self.lead_dataframe.price.min()
-        max_price = self.lead_dataframe.price.max()
-        trade_fee = trade["price"] * (self.lag_exchange.taker_fee *2)
-        # this should be moved down to seperate function
-        if (max_price - min_price) > trade_fee:
-            min_pos = self.lead_dataframe.price.idxmin()
-            max_pos = self.lead_dataframe.price.idxmax()
-            if not self.trade_locked:
-                if min_pos < max_pos:
-                    target_price = self.lag_price * (1.0 + self.lag_exchange.taker_fee *2)
-                    print("Will Buy")
-                    self.lag_exchange.new_order(
-                        "market",
-                        "XBTUSD",
-                        0.0,
-                        1.0,
-                        True
-                    )
-                    self.lag_exchange.new_order(
-                        "limit",
-                        "XBTUSD",
-                        target_price,
-                        -1.0,
-                        True
-                    )
-                else:
-                    target_price = self.lag_price * (1.0 - self.lag_exchange.taker_fee *2)
-                    print("Will Sell")
-                    self.lag_exchange.new_order(
-                        "market",
-                        "XBTUSD",
-                        0.0,
-                        -1.0,
-                        True
-                    )
-                    self.lag_exchange.new_order(
-                        "limit",
-                        "XBTUSD",
-                        target_price,
-                        1.0,
-                        True
-                    )
-                self.trade_locked = True
-        else:
-            if self.trade_locked:
-                print("will reset")
-            self.trade_locked = False
-            
-#         print(f"lead: {self.lead_price}")
+        if self.lag_price:
+            self.check_difference()
     
     def update_lag(self, trade):
         self.lag_price = trade["price"]
@@ -99,6 +52,71 @@ class Strategy():
         self.lag_dataframe = pd.DataFrame(
             self.lag_price_list
         ).set_index("datetime", drop=True).last(self.time_diff)
+        self.lag_exchange.datafeed(trade)
+
+    def create_position(self, target_price, amount):
+        self.lag_exchange.new_order(
+            "market",
+            "XBTUSD",
+            0.0,
+            amount,
+            True
+        )
+        self.order_id = self.lag_exchange.new_order(
+            "limit",
+            "XBTUSD",
+            target_price,
+            -amount,
+            True
+        )
+
+    def move_position(self, target_price, amount):
+        self.lag_exchange.update_order(
+            self.order_id,
+            target_price,
+            -amount
+        )
+        print(f"Updated order: {self.lag_exchange.orders[-1].price}" )
+        
+    def close_position(self, size):
+        self.lag_exchange.new_order(
+            "market",
+            "XBTUSD",
+            0.0,
+            size,
+            True
+        )
+        self.lag_exchange.cancel_order(
+            self.order_id
+        )
         
     def check_difference(self):
-        pass
+        min_price = self.lead_dataframe.price.min()
+        max_price = self.lead_dataframe.price.max()
+        trade_fee = self.lag_price * (self.lag_exchange.taker_fee *2)
+        positions = self.lag_exchange.positions
+        if ((max_price - min_price) > trade_fee):
+            min_pos = self.lead_dataframe.price.idxmin()
+            max_pos = self.lead_dataframe.price.idxmax()
+            if min_pos < max_pos:
+                target_price = self.lag_price * (1.0 + self.target_percentage)
+                if not positions:
+                    print("Will Buy")
+                    self.create_position(target_price, 1.0)
+                elif (positions["XBTUSD"].size > 0.0) and target_price > self.lag_exchange.orders[-1].price:
+                    self.move_position(target_price, 1.0)
+                elif positions["XBTUSD"].size < 0.0:
+                    self.close_position(-positions["XBTUSD"].size)
+                    print("Closed old position. Will Buy")
+                    self.create_position(target_price, 1.0)
+            else:
+                target_price = self.lag_price * (1.0 - self.target_percentage)
+                if not positions:
+                    print("Will Sell")
+                    self.create_position(target_price, -1.0)
+                elif (positions["XBTUSD"].size < 0.0) and target_price < self.lag_exchange.orders[-1].price: # Add logic for only updating positive direction.
+                    self.move_position(target_price, -1.0)
+                elif positions["XBTUSD"].size > 0.0:
+                    self.close_position(-positions["XBTUSD"].size)
+                    print("Closed old position. Will Sell")
+                    self.create_position(target_price, -1.0)
